5158f4af5b9dc2755fe2f1829be196295e670e9f
andreasalvucci <andrea.salvucci@studio.unibo.it>
2022-05-02 18:05:45 +0200

    secondo commit

--- src/server.js
+++ src/server.js
@@ -4,28 +4,38 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 const app = express()
 
+app.use(cookieParser)
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
-     return
 })
 
 app.get('/QpC9ZvbcR79G3HHd', (req, res) => {
-    res.status(404).json({message: "1"})
+    var primoTassello = req.cookies.primoTassello;
+    if(primoTassello == undefined){
+        res.cookie('primoTassello',"QpC9ZvbcR79G3HHd", { maxAge: 900000, httpOnly: true });
+    }
+    res.status(200).json({message:"Complimenti! Hai trovato il primo tassello"})
     return
 })
 
 app.get('/uxDsnMVWsrM8S5KE', (req, res) => {
-    res.status(404).json({message: "2"})
+    var primoTassello = req.cookies.primoTassello;
+    if(primoTassello == undefined){
+        res.status(401).json({message:"Spiacenti, devi prima collezionare il primo tassello"})
+        return
+    }
+    if(primoTassello == "QpC9ZvbcR79G3HHd"){
+        res.status(200).json({message:"Complimenti! Hai trovato il secondo tassello!"})
+    }
     return
 })
 
 app.get('/3ULtNzytBQAJzkUR', (req, res) => {
-    res.status(404).json({message: "3"})
     return
 })
 
 app.get('/d9JxqHn9z5pBjqvp', (req, res) => {
-    res.status(404).json({message: "4"})
     return
 })
 
