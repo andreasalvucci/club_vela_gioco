c4abd45dc4a8cfd1f1402fcae5c04e193b124f23
andreasalvucci <andrea.salvucci@studio.unibo.it>
2022-05-02 18:33:20 +0200

    Prima bozza semi-funzionante

--- src/server.js
+++ src/server.js
@@ -3,17 +3,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 const app = express()
+const BACKEND_PORT = 8080
 
-app.use(cookieParser)
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    console.log("ARRIVATA REQUEST")
+    return res.send({
+        timestamp: new Date().getTime(),
+    })
+    
 })
 
 app.get('/QpC9ZvbcR79G3HHd', (req, res) => {
     var primoTassello = req.cookies.primoTassello;
     if(primoTassello == undefined){
         res.cookie('primoTassello',"QpC9ZvbcR79G3HHd", { maxAge: 900000, httpOnly: true });
+        return
     }
     res.status(200).json({message:"Complimenti! Hai trovato il primo tassello"})
     return
@@ -26,19 +32,45 @@ app.get('/uxDsnMVWsrM8S5KE', (req, res) => {
         return
     }
     if(primoTassello == "QpC9ZvbcR79G3HHd"){
+        res.cookie('secondoTassello','uxDsnMVWsrM8S5KE', { maxAge: 900000, httpOnly: true })
         res.status(200).json({message:"Complimenti! Hai trovato il secondo tassello!"})
+        
+        return
+    }
+})
+
+app.get('/3ULtNzytBQAJzkUR', (req, res) => {
+
+    var primoTassello = req.cookies.primoTassello;
+    var secondoTassello = req.cookies.secondoTassello;
+    if(secondoTassello == undefined || primoTassello == undefined){
+        
+        res.status(401).json({message:"Spiacenti, devi prima collezionare il secondo tassello"})
+
+    }
+    if(primoTassello == "QpC9ZvbcR79G3HHd" && secondoTassello == "uxDsnMVWsrM8S5KE"){
+        res.cookie('terzoTassello','3ULtNzytBQAJzkUR', { maxAge: 900000, httpOnly: true })
+        res.status(200).json({message:"Complimenti! Hai trovato il terzo tassello"})
     }
     return
 })
 
-app.get('/3ULtNzytBQAJzkUR', (req, res) => {
-    return
-})
-
 app.get('/d9JxqHn9z5pBjqvp', (req, res) => {
+    var primoTassello = req.cookies.primoTassello;
+    var secondoTassello = req.cookies.secondoTassello;
+    var terzoTassello = req.cookies.terzoTassello;
+    if(secondoTassello == undefined || primoTassello == undefined || terzoTassello == undefined){
+        
+        res.status(401).json({message:"Spiacenti, devi prima collezionare il terzo tassello"})
+
+    }
+
+    if(primoTassello == "QpC9ZvbcR79G3HHd" && secondoTassello == "uxDsnMVWsrM8S5KE" && terzoTassello =="3ULtNzytBQAJzkUR"){
+        res.status(200).json({message:"Complimenti! Hai trovato tutti i tasselli, scopri ora il tuo premio!"})
+    }
     return
 })
 
-app.listen(8080, async () => {
-    console.log(`HTTP Server listening on port ${process.env.PORT}...`);
+app.listen(BACKEND_PORT, async () => {
+    console.log("HTTP Server listening on port " + BACKEND_PORT+"...");
 });
